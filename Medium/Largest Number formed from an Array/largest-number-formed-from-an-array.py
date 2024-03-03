e01b07434983d556bfdd00e27801a0e524114d27
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-03-03 16:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+from functools import cmp_to_key
+
+class Solution:
+
+	def printLargest(self, n, arr):
+	    # code here
+	    return str(int("".join(sorted(arr, key= cmp_to_key(lambda a,b : -1 if (a+b) > (b+a) else 1 )))))
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import functools
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(str, input().strip().split()))
+        ob = Solution()
+        ans = ob.printLargest(n, arr)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
