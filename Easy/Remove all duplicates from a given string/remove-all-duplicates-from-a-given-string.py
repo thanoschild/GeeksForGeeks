e5d1b48002366d966cecb2582b818d281857a486
Sumeet Haldar <99740668+thanoschild@users.noreply.github.com>
2024-03-10 20:18:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove all duplicates from a given string/remove-all-duplicates-from-a-given-string.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+class Solution:
+
+	
+	def removeDuplicates(self,str):
+	    # code here
+	    st = set()
+	    ans = ""
+	    for c in str:
+	        if c not in st:
+	            ans += c
+	            st.add(c)
+	            
+	    return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        str = input().strip()
+        ob = Solution()
+        ans = ob.removeDuplicates(str)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
